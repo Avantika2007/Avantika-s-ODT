f47d0cfe3c4faa42c6816c85b84b02925517b9b3
Avantika2007 <avantika.vinod2025@bitsdesign.edu.in>
2026-02-16 23:28:43 +0530

    Create Random.py

---
+++ Week 5/Random.py
@@ -0,0 +1,8 @@
+import random
+import time
+while True:
+  x=random.randint(0,15)
+  print(x)
+  time.sleep(1)
+
+'''If initilisation does not take place in the while loop, then a random number will be selected and only that will be printed multiple times'''
