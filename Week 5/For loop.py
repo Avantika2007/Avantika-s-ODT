ad010c0edbbf70d9b9e959bbf7fcaa12c937559a
Avantika2007 <avantika.vinod2025@bitsdesign.edu.in>
2026-02-16 23:24:54 +0530

    Create For loop.py

---
+++ Week 5/For loop.py
@@ -0,0 +1,30 @@
+for i in range (5):
+  print('Avantika')
+#OUTPUT: Avantika is printed 5 times
+#-----------------------------------
+for in range (5):
+print(i)
+'''OUTPUT: 
+0
+1
+2
+3
+4'''
+#-----------------------------------
+for i in range (5):
+  i=i+1
+  print(i)
+'''OUTPUT: 
+1
+2
+3
+4
+5'''
+#-----------------------------------
+for i in range (5,11,2):
+  print(i)
+'''OUTPUT: 
+5
+7
+9'''
+#-----------------------------------
