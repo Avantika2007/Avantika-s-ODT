65ca2ff150887cd8dfba2708e46fd4208de140fb
Avantika2007 <avantika.vinod2025@bitsdesign.edu.in>
2026-02-16 21:29:17 +0530

    Update To light internal LED.py

--- Week 3/To light internal LED.py
+++ Week 3/To light internal LED.py
@@ -2,3 +2,12 @@
 from machine import Pin
 led=Pin(2, Pin.OUT) #Pin class is input by default; 2 is port no.
 led.on() #switched led on
+#--------------------------
+#LED with delay
+from machine import Pin
+import time #importing time module
+led=Pin(2,Pin.OUT)
+led.on()
+time.sleep(0.2)
+led.off()
+time.sleep(0.2)
