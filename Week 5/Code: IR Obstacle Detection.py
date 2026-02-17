12148bd577ae2805812c7f75f62f157ab54885d7
Avantika2007 <avantika.vinod2025@bitsdesign.edu.in>
2026-02-17 18:12:33 +0530

    Create Code: IR Obstacle Detection.py

---
+++ Week 5/Code: IR Obstacle Detection.py
@@ -0,0 +1,11 @@
+from machine import Pin
+import time
+s=Pin(14, Pin.IN, Pin.PULL_UP)
+led=Pin(23, Pin.OUT)
+while True:
+  s_value=s.value()
+  if s_value==0:
+    led.on()
+    print('Obstacle Detection')
+  else:
+    led.off()
