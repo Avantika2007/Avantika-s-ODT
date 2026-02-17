2ebd2b4364d17736828fa185c90c48cf01e3ad02
Avantika2007 <avantika.vinod2025@bitsdesign.edu.in>
2026-02-17 17:55:56 +0530

    Update Random and Neopixel.py

--- Week 5/Random and Neopixel.py
+++ Week 5/Random and Neopixel.py
@@ -1,1 +1,15 @@
+#To light up a random LED
+from machine import Pin
+import time
+import neopixel
+import random
+x=neopixel.NeoPixel(Pin(12),16)
+while True:
+  r=random.randint(1,16)
+  time.sleep(1)
+  print(r)
+  x[5]=(255,0,255)
+  time.sleep(0.2)
+  x.write()
 
+#-------------------------
